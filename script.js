3b63d3e50ec4941f3c398be7a58f6c3462ef6667
Rita Madio <160418348+ritamad@users.noreply.github.com>
2024-07-30 18:35:44 +0200

    add size and reset buttons - style buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const sqCont = document.querySelector('#squares-container');
-const squareDiv = document.createElement('div');
-squareDiv.classList.add('square');
+// const sqCont = document.querySelector('#squares-container');
+// const squareDiv = document.createElement('div');
+// squareDiv.classList.add('square');
 
-sqCont.appendChild(squareDiv);
+// sqCont.appendChild(squareDiv);
