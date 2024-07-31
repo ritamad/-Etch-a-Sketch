0d1b34329e5642fe4390fb444f4fd67c21d48b12
Rita Madio <160418348+ritamad@users.noreply.github.com>
2024-07-31 18:57:11 +0200

    add function to ask grid size - add event handler to get the grid size and create it - start function to create the grid;

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-// const sqCont = document.querySelector('#squares-container');
+// const sqCont = document.querySelector('#grid-container');
 // const squareDiv = document.createElement('div');
 // squareDiv.classList.add('square');
 
 // sqCont.appendChild(squareDiv);
+
+//FUNCTION TO ASK THE GRID SIZE
+function askSize() {
+    const size = prompt('choose the grid rows and columns number (max 100)');
+    return size;
+};
+
+// EVENT HANDLER TO GET THE GRID SIZE AND CREATE IT
+const sizeBtn = document.querySelector('#size');
+sizeBtn.focus();
+sizeBtn.addEventListener('click', () => {
+    const size = askSize();
+    if (size !== null) {
+        createGrid(size);
+    }
+});
+
+//FUNCTION TO CREAT THE GRID
+function createGrid(size) {
+
+}
